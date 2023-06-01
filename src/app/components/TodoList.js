a89a35684e9af45b64081700fb27c42da5e5d964
Matthew Jericho Sy <matthew_sy@dlsu.edu.ph>
2023-06-01 11:32:52 +0800

    useReducer instead of useState

--- src/app/components/TodoList.js
+++ src/app/components/TodoList.js
@@ -1,26 +1,38 @@
 "use client";
 
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import ToDo from "./Todo";
 import AddToDo from "./AddToDo";
 
 let nextId = 1;
 
+function todosReducer(todos = [], action) {
+    switch (action.type) {
+        case "add": {
+            return [...todos, { id: action.id, title: action.title, done: false }];
+        }
+        case "change": {
+            return todos.map(todo => ({
+                ...todo, ...(action.id !== todo.id ? {} : { [action.property]: action.newValue })
+            }))
+        }
+        case "delete": {
+            return todos.filter(todo => todo.id !== action.id)
+        }
+        default: throw Error(`Unknown action: ${action.type}`);
+    }
+}
+
 export default function TodoList() {
-    const [todos, setTodos] = useState([]);
+    const [todos, dispatch] = useReducer(todosReducer, []);
     function handleAddToDo(title) {
-        setTodos([...todos, { id: nextId++, title, done: false }]);
+        dispatch({ type: 'add', id: nextId++, title });
     }
     function handleChangeToDo(id, property, newValue) {
-        setTodos(
-            todos.map((todo) => ({
-                ...todo,
-                ...(id !== todo.id ? {} : { [property]: newValue }),
-            }))
-        );
+        dispatch({ type: 'change', id, property, newValue });
     }
     function handleDeleteToDo(id) {
-        setTodos(todos.filter((todo) => todo.id !== id));
+        dispatch({ type: 'delete', id });
     }
 
     return (
