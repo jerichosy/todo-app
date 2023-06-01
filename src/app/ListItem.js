3f94f8b05812fe3aee9a8108dca5064eaed3fa41
Matthew Jericho Sy <matthew_sy@dlsu.edu.ph>
2023-06-01 20:09:21 +0800

    Update ListItem.js (bug here)
    This check doesn't work properly

--- src/app/ListItem.js
+++ src/app/ListItem.js
@@ -6,6 +6,13 @@ import { LevelContext } from './context/LevelContext';
 
 export default function ListItem({ children }) {
   const level = useContext(LevelContext);
+  console.log(level)
+  if (level === 0) {
+    throw Error('List item must be inside a list!');
+  }
+  if (level > 4) {
+    throw Error('Unsupported level: ' + level);
+  }
   return (
     <li className={`list-item-${level}`}>{children}</li>
   )
